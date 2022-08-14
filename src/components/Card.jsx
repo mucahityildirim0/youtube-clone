20f0cb228172774ac050e02c1de24328924c2b75
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-08-14 10:48:42 +0300

    styled components using props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Card = () => {
+const Card = ({ type }) => {
   return (
     <Link to="/video/test" style={{ textDecoration: "none" }}>
-      <Container>
-        <Image src="https://www.wallpapers.net/web/wallpapers/beautiful-water-house-nature/2560x1440.jpg" />
-        <Details>
+      <Container type={type}>
+        <Image
+          type={type}
+          src="https://www.wallpapers.net/web/wallpapers/beautiful-water-house-nature/2560x1440.jpg"
+        />
+        <Details type={type}>
           <ChannelImage src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
           <Texts>
             <Title>Test Video</Title>
@@ -21,23 +24,26 @@ const Card = () => {
 };
 
 const Container = styled.div`
-  width: 360px;
-  margin-bottom: 45px;
+  width: ${(props) => props.type !== "sm" && "none"};
+  margin-bottom: ${(props) => (props.type === "sm" ? "10px" : "45px")};
   cursor: pointer;
+  display: ${(props) => props.type === "sm" && "flex"};
   gap: 10px;
+  background-color: red;
 `;
 
 const Image = styled.img`
   width: 100%;
-  height: 202px;
+  height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
   background-color: #999;
   flex: 1;
 `;
 
 const Details = styled.div`
   display: flex;
-  margin-top: 16px;
+  margin-top: ${(props) => props.type !== "sm" && "16px"};
   gap: 12px;
+  flex: 1;
 `;
 
 const ChannelImage = styled.img`
@@ -45,6 +51,7 @@ const ChannelImage = styled.img`
   height: 36px;
   border-radius: 50%;
   background-color: #999;
+  display: ${(props) => props.type === "sm" && "none"};
 `;
 
 const Texts = styled.div``;
